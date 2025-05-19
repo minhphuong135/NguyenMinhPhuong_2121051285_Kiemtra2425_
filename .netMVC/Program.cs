698fdbe479f129f844c715ce0b0a65e95891771f
Nguyen Minh Phuong <nguyenminhp135@gmail.com>
2025-05-19 09:47:13 +0700

    KiemtraB

--- .netMVC/Program.cs
+++ .netMVC/Program.cs
@@ -1,5 +1,4 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
 
 
 // Add services to the container.
