b2abf932e684bd803d54ef399c91a6e079626d5a
Nguyen Minh Phuong <nguyenminhp135@gmail.com>
2025-05-26 08:01:36 +0700

    kiemtraB

--- DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7702200907df7c4c02b05a0bc3ec605e738407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac17c50f7ca20967654638e5d7f840a4737f29c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
